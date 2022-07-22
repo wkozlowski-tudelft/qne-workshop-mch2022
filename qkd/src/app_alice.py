2bf881c7d49db1ff2512a38cdb57279e20dbba51
Wojciech Kozlowski <w.kozlowski@tudelft.nl>
2022-07-22 09:26:45 +0200

    Application example

---
+++ qkd/src/app_alice.py
@@ -0,0 +1,88 @@
+import logging
+import random
+
+from netqasm.sdk.classical_communication.message import StructuredMessage
+from netqasm.sdk.external import NetQASMConnection, Socket
+from netqasm.sdk.epr_socket import EPRSocket
+
+logger = logging.getLogger(__name__)
+
+def main(app_config=None, x=0, y=0):
+    handler = logging.FileHandler("alice.log", encoding="utf-8")
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    epr_pairs = 128
+
+    socket = Socket("alice", "bob", log_config=app_config.log_config)
+    epr_socket = EPRSocket("bob")
+
+    conn = NetQASMConnection(
+        app_name=app_config.app_name,
+        log_config=app_config.log_config,
+        epr_sockets=[epr_socket],
+    )
+
+    raw_key = []
+    apply_h = []
+
+    with conn:
+        logger.debug("START")
+
+        # Stage 1: Receiving
+        logger.debug("Stage 1: Receiving")
+        for i in range(epr_pairs):
+            qubit = epr_socket.create_keep(1)[0]
+
+            if bool(random.randint(0, 1)):
+                apply_h.append(True)
+                qubit.H()
+            else:
+                apply_h.append(False)
+
+            m = qubit.measure()
+            conn.flush()
+
+            logger.debug(f"Pair {i}; Apply H: {apply_h[-1]}; Measured bit: {m}")
+            raw_key.append(int(m))
+
+        logger.debug(f"Raw key: {raw_key}")
+
+        # Stage 2: Sifting
+        logger.debug(f"Local H: {apply_h}")
+        socket.send_structured(StructuredMessage("H", apply_h))
+
+        remote_apply_h = socket.recv_structured().payload
+        logger.debug(f"Remote H: {remote_apply_h}")
+
+        sifted_key = []
+        for i, (local_h, remote_h) in enumerate(zip(apply_h, remote_apply_h)):
+            if local_h == remote_h:
+                sifted_key.append(raw_key[i])
+
+        logger.debug(f"Sifted key: {sifted_key}")
+
+        # Stage 3: Security
+        num_security_bits = max(len(sifted_key) // 4, 1)
+
+        security_bits = sifted_key[:num_security_bits]
+
+        logger.debug(f"Local security bits: {security_bits}")
+        socket.send_structured(StructuredMessage("Security bits", security_bits))
+
+        remote_security_bits = socket.recv_structured().payload
+        logger.debug(f"Remote security bits: {remote_security_bits}")
+
+        if security_bits != remote_security_bits:
+            logger.warning("Eavesdropper detected!")
+            key = None
+        else:
+            key = sifted_key[num_security_bits:]
+
+        logger.debug(f"Final key: {key}")
+
+    return {"key": key}
+
+
+if __name__ == "__main__": 
+    main()
